0aaf1dbde8811b498a464c61363b8019e544aafa
ThousandDucks <edison03lee@gmail.com>
2026-08-02 00:17:40 +0100

    Prepare File Vault for deployment

---
+++ utils/formatFileSize.js
@@ -0,0 +1,10 @@
+function formatFileSize(bytes) {
+    if (bytes === 0) return "0 Bytes";
+
+    const units = ["Bytes", "KB", "MB", "GB", "TB"];
+    const index = Math.floor(Math.log(bytes) / Math.log(1024));
+
+    return `${(bytes / Math.pow(1024, index)).toFixed(1)} ${units[index]}`;
+}
+
+module.exports = formatFileSize;
